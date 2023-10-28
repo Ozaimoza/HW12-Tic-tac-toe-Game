8728fb001090d9890efd7675de0d5824719a5ab7
Ozaimoza <mozaikramadhan97@gmail.com>
2023-10-28 22:49:52 +0700

    format app js prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import * as React from 'react';
-import { ChakraProvider  } from '@chakra-ui/react';
-import {  Button, Card, CardHeader, CardBody, CardFooter, Heading, SimpleGrid, Text, Grid, GridItem, Box} from '@chakra-ui/react'
-
-
-
-
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  SimpleGrid,
+  Text,
+  Grid,
+  GridItem,
+  Box,
+} from "@chakra-ui/react";
 
 function Board() {
   const [squares, setSquares] = React.useState(Array(9).fill(null));
@@ -20,7 +28,7 @@ function Board() {
     newSquares[square] = calculateNextValue(squares);
 
     if (calculateWinner(newSquares)) {
-      calculateNextValue(squares) === 'X'
+      calculateNextValue(squares) === "X"
         ? setXScore(xScore + 1)
         : setOScore(oScore + 1);
     }
@@ -56,7 +64,7 @@ function Board() {
     }
 
     return (
-    <Button
+      <Button
         w="100%"
         h="110px"
         fontSize="80px"
@@ -65,130 +73,151 @@ function Board() {
         border="4px"
         backgroundColor={backgroundColor}
         borderRadius="lg"
-        borderColor= {borderColor}
+        borderColor={borderColor}
         onClick={onClick}
-    >
-        <Text textAlign="center" color={textColor}>{value}</Text>
-    </Button>
+      >
+        <Text textAlign="center" color={textColor}>
+          {value}
+        </Text>
+      </Button>
     );
-}
-
+  }
 
   function renderSquare(i) {
     return (
-      <ChangeColorButton value={squares[i]} onClick={() => selectSquare(i)} boxShadow='inner'/>
+      <ChangeColorButton
+        value={squares[i]}
+        onClick={() => selectSquare(i)}
+        boxShadow="inner"
+      />
     );
   }
 
   return (
-  <Box
-
-  >
-    <Card h='calc(100vh)' align='center'
-        backgroundColor="teal.200"
-    >
-      <Grid   
-        templateRows='repeat(2, 1fr)'
-        templateColumns='repeat(5, 1fr)'
-        gap={6}
-      >
-        <GridItem pt = "150%" rowSpan={2} colSpan={1} >
-            <Box 
-                align = "center"
-                borderRadius="lg"
-                border="4px"
-                borderColor="red.500"
-                fontSize="20px"
-                fontWeight="bold"
-                boxShadow='dark-lg'
-              >
-
-              <Text  backgroundColor="red.200">Player X Score</Text>
-              <Text 
-                fontWeight="bold"
-                backgroundColor="gray.200"
-                fontSize='70px'
-              >
-                  {xScore}
-              </Text>
-            </Box>
-        </GridItem>
-
-        <GridItem rowSpan={2} colSpan={3} >
-
-          <CardHeader align='center'>
-            <Heading size='md' fontSize="40px" fontWeight="bold" textShadow= '0 0 2px black'> {calculateStatus(calculateWinner(squares), squares, calculateNextValue(squares))} </Heading>
-          </CardHeader>
-          
-          <CardBody align='center'>
-          
-            <SimpleGrid 
-            columns={3} 
-            spacing={10}
-            backgroundColor="gray.200" 
-            boxShadow='dark-lg'
-            borderRadius="lg"
-            padding={5}
+    <Box>
+      <Card h="calc(100vh)" align="center" backgroundColor="teal.200">
+        <Grid
+          templateRows="repeat(2, 1fr)"
+          templateColumns="repeat(5, 1fr)"
+          gap={6}
+        >
+          <GridItem pt="150%" rowSpan={2} colSpan={1}>
+            <Box
+              align="center"
+              borderRadius="lg"
+              border="4px"
+              borderColor="red.500"
+              fontSize="20px"
+              fontWeight="bold"
+              boxShadow="dark-lg"
             >
-              {renderSquare(0)}
-              {renderSquare(1)}
-              {renderSquare(2)}
-            
-              {renderSquare(3)}
-              {renderSquare(4)}
-              {renderSquare(5)}
-            
-              {renderSquare(6)}
-              {renderSquare(7)}
-              {renderSquare(8)}
-            </SimpleGrid>
-
-          </CardBody>
-
-          <CardFooter align='center'>
-            <SimpleGrid w="100%" columns={2} spacing={10}>
-              <Button h="100px" colorScheme='yellow' onClick={restart} boxShadow='dark-lg' fontSize="40px" fontWeight="bold">
-                RESTART
-              </Button>
-              <Button h="100px" colorScheme='green' onClick={startNewGame} boxShadow='dark-lg' fontSize="40px" fontWeight="bold">
-                NEXT
-              </Button>
-            </SimpleGrid>
-          </CardFooter>
-
-        </GridItem>
-
-        <GridItem pt = "150%" rowSpan={2} colSpan={1} >
-            <Box 
-                align = "center"
-                borderRadius="lg"
-                border="4px"
-                borderColor="green.500"
-                fontSize="20px"
-                fontWeight="bold"
-                boxShadow='dark-lg'
-              >
-
-              <Text  backgroundColor="green.200">Player O Score</Text>
-              <Text 
+              <Text backgroundColor="red.200">Player X Score</Text>
+              <Text
                 fontWeight="bold"
                 backgroundColor="gray.200"
-                fontSize='70px'
+                fontSize="70px"
               >
-                  {oScore}
+                {xScore}
               </Text>
             </Box>
-        </GridItem>
-      </Grid>
-    </Card>
-  </Box>
+          </GridItem>
+
+          <GridItem rowSpan={2} colSpan={3}>
+            <CardHeader align="center">
+              <Heading
+                size="md"
+                fontSize="40px"
+                fontWeight="bold"
+                textShadow="0 0 2px black"
+              >
+                {" "}
+                {calculateStatus(
+                  calculateWinner(squares),
+                  squares,
+                  calculateNextValue(squares)
+                )}{" "}
+              </Heading>
+            </CardHeader>
+
+            <CardBody align="center">
+              <SimpleGrid
+                columns={3}
+                spacing={10}
+                backgroundColor="gray.200"
+                boxShadow="dark-lg"
+                borderRadius="lg"
+                padding={5}
+              >
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+              </SimpleGrid>
+            </CardBody>
+
+            <CardFooter align="center">
+              <SimpleGrid w="100%" columns={2} spacing={10}>
+                <Button
+                  h="100px"
+                  colorScheme="yellow"
+                  onClick={restart}
+                  boxShadow="dark-lg"
+                  fontSize="40px"
+                  fontWeight="bold"
+                >
+                  RESTART
+                </Button>
+                <Button
+                  h="100px"
+                  colorScheme="green"
+                  onClick={startNewGame}
+                  boxShadow="dark-lg"
+                  fontSize="40px"
+                  fontWeight="bold"
+                >
+                  NEXT
+                </Button>
+              </SimpleGrid>
+            </CardFooter>
+          </GridItem>
+
+          <GridItem pt="150%" rowSpan={2} colSpan={1}>
+            <Box
+              align="center"
+              borderRadius="lg"
+              border="4px"
+              borderColor="green.500"
+              fontSize="20px"
+              fontWeight="bold"
+              boxShadow="dark-lg"
+            >
+              <Text backgroundColor="green.200">Player O Score</Text>
+              <Text
+                fontWeight="bold"
+                backgroundColor="gray.200"
+                fontSize="70px"
+              >
+                {oScore}
+              </Text>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Card>
+    </Box>
   );
 }
 
 function Game() {
   return (
-    <div >
-      <div >
+    <div>
+      <div>
         <Board />
       </div>
     </div>
@@ -206,7 +235,7 @@ function calculateStatus(winner, squares, nextValue) {
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+  return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
